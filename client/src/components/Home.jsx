3c4226b14ce9e0568ccbc48eecea43b0577e4a62
domniea <domniea@gmail.com>
2023-06-09 09:43:58 -0600

    New Homepage and Readme

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -25,3 +25,5 @@ function Home() {
 }
 
 export default Home
+
+We are thrilled to present our collective efforts in this project,  brought to you by Adam Domnie, Mark Faulkner, and Angela Ellsworth.  With our newfound expertise in frontend and backend development, we have crafted an innovative and dynamic web solution that showcases our skills and passion for creating exceptional digital experiences. Join us on this exciting journey as we explore the limitless possibilities of full stack web development. 
