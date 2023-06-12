eefac913f0352752f931b5ea03a56db46d8e7d18
domniea <domniea@gmail.com>
2023-06-11 20:17:39 -0600

    test

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://dog-walker-project.herokuapp.com/dogs',
+        target: 'https://dog-walker-project.herokuapp.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
 
